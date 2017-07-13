cc17d46cf80650a80ccddf10ec0738f96ad626a9
MajorIdiocy <nicholas.boyd@sjsu.edu>
2017-07-12 21:51:36 -0700

    Big Update
    Desktop Copy

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@ import jinja2
 
 def init():
     global env
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(["public/html", "public/css", "public/txt"]))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(["public/html", "public/css"]))
     global header
     header = env.get_template("header.html")
     global pkmnpage
-    pkmnpage = env.get_template("pkmn.html")
+    pkmnpage = env.get_template("pokemon.html")
+    global navbar
+    navbar = env.get_template("navbar.html")
 
 def renderHeader(title="Website", desc=""):
     return header.render(title=title, desc=desc)
