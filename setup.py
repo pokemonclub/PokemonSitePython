775d2ad648c7123819347386296c9af910c2cdff
pokemonclub <pokemonpres@gmail.com>
2017-07-13 13:47:30 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import jinja2
 
 def init():
     global env
-    env = jinja2.Environment(loader=jinja2.FileSystemLoader(["public/html", "public/css"]))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(["public/html"]))
     global header
     header = env.get_template("header.html")
     global pkmnpage
